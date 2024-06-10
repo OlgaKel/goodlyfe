a59fdac552afab2911acd3d05f144414dd277c56
OlgaKel <nikkelos@ya.ru>
2024-06-10 18:34:37 +0300

    adaptive header add

--- src/js/index.js
+++ src/js/index.js
@@ -7,7 +7,23 @@ document.addEventListener('DOMContentLoaded',function(){
     headerSearchForm.classList.toggle('search-form--open')
 
 });
+/*====== menu burger click ====*/
 
+const menuToggle = document.querySelector('#menu-toggle');
+const mobileMenu = document.querySelector('#header-menu');
+const bodyEl=document.body;
+if(menuToggle){
+  menuToggle.addEventListener('click',()=>{
 
+   if(menuToggle.classList.contains('active')){
+    
+     menuToggle.classList.remove('active');
+     mobileMenu.classList.remove('active');
+   }else{
+     menuToggle.classList.add('active');
+     mobileMenu.classList.add('active');
+     }
+  });
+}
 
 })
